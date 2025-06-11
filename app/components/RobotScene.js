13432d49a60acd167d433247d29148f19d6d9be7
Khaled <cto@wappdev.com>
2025-06-11 17:22:29 +0100

    folden name models changed

--- app/components/RobotScene.js
+++ app/components/RobotScene.js
@@ -1,18 +1,21 @@
 
 "use client";
+import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
 import AnimatedRobot from "./AnimatedRobot";
+
 
 export default function RobotScene() {
   return (
     <Canvas camera={{ position: [0, 1, 5], fov: 50 }}>
       <ambientLight intensity={0.5} />
       <directionalLight position={[2, 2, 2]} />
-      <OrbitControls enableZoom={false} />
 
       <Suspense fallback={null}>
         <AnimatedRobot scale={3} position={[0, -1, 0]} />
+        <OrbitControls enableZoom={false} autoRotate autoRotateSpeed={2} />
+
       </Suspense>
       
     </Canvas>
