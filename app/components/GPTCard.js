59daf603c47d64f1fdf485f5a5fa306534fe8273
Khaled <cto@wappdev.com>
2025-06-11 12:31:13 +0100

    adding CV

--- app/components/GPTCard.js
+++ app/components/GPTCard.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import FakeGPTChatBox from "./FakeGPTChatBox";
 import PortfolioContent from "./PortfolioContent";
 import HighlightWord from "./HighlightWord";
