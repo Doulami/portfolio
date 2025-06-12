b870779157dc2fc0a73cc36313d93d3378ed86cf
Khaled <cto@wappdev.com>
2025-06-12 11:34:34 +0100

    robot right side

--- app/components/AnimatedRobot.js
+++ app/components/AnimatedRobot.js
@@ -11,32 +11,5 @@ export default function AnimatedRobot(props) {
     actions[Object.keys(actions)[0]]?.play(); // auto-play first animation
   }, [actions]);
 
-  return (
-    <group {...props} ref={group}>
-
-      {/* Glow floor under robot */}
-      <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -1.3, 0]}>
-        <circleGeometry args={[2.5, 64]} />
-        <meshBasicMaterial color="#00CFFF" transparent opacity={0.08} />
-      </mesh>
-
-      {/* Background glow sphere */}
-      <mesh position={[0, 0.1, -1.5]}>
-        <sphereGeometry args={[3.2, 64, 64]} />
-        <meshBasicMaterial color="#00CFFF" transparent opacity={0.1} />
-      </mesh>
-
-      {/* Main robot model */}
-      <primitive object={scene} />
-
-      {/* Glow light */}
-      <pointLight
-        color="#00CFFF"
-        intensity={1.2}
-        distance={6}
-        position={[0, 1.5, 2.5]}
-        castShadow={false}
-      />
-    </group>
-  );
+  return <primitive ref={group} object={scene} {...props} />;
 }
