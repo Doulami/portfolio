048be6390c9ce00ee1ccb41150d2b647ff1bd6e6
Khaled <cto@wappdev.com>
2025-06-13 15:12:02 +0100

    robot right side button cv

--- app/components/CardLeft.js
+++ app/components/CardLeft.js
@@ -1,9 +1,6 @@
 "use client";
 
 import { useEffect, useState } from "react";
-"use client";
-
-import { useEffect, useState } from "react";
 import IntroPanel from "./IntroPanel";
 import PortfolioContent from "./PortfolioContent";
 
